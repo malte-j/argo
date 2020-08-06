dbd64245ebbc60f97af09ecd9ffaea0e53c09337
Malte <skyguardian42@gmail.com>
2020-08-06 18:27:28 +0200

    Animated Navbar

--- themes/argo/page-home.php
+++ themes/argo/page-home.php
@@ -1,12 +1,18 @@
 <?php
 get_header();
 ?>
-<header class="landing--header">
-<h1><?php the_field('intro_text'); ?></h1>
+<header class="landing--header"> 
+  <h1><?php the_field('intro_text'); ?></h1>
+  <?php the_header_image_tag(); ?>
 </header>
-<h1>some text:</h1>
+<h1>blub</h1>
+<h1>blub</h1>
+<h1>blub</h1>
+<h1>blub</h1>
+<h1>blub</h1>
+<h1>blub</h1>
+
 cool.
-<?php the_header_image_tag(); ?>
 <p style="color: <?php the_field('services'); ?>"></p>
 
 <?php get_footer(); ?>
