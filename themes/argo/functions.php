b365414fbce18f032a37c2680ad0490d027311c7
Malte <skyguardian42@gmail.com>
2020-08-18 20:59:42 +0200

    fix css link in wordpress

--- themes/argo/functions.php
+++ themes/argo/functions.php
@@ -146,7 +146,7 @@ add_action( 'widgets_init', 'argo_widgets_init' );
  */
 function argo_scripts() {
 	wp_enqueue_style( 'argo-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_enqueue_style('main-styles', get_template_directory_uri() . '/css/main-style.css', array(), filemtime(get_template_directory() . '/css/main-style.css'), false);
+	wp_enqueue_style('main-styles', get_template_directory_uri() . '/dist/bundle.css', array(), filemtime(get_template_directory() . '/dist/bundle.css'), false);
 
 	wp_style_add_data( 'argo-style', 'rtl', 'replace' );
 
