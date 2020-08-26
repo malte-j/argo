6434c4de430e1ce1735349ecfcecf1dba18a67f2
Malte <skyguardian42@gmail.com>
2020-08-26 13:42:52 +0200

    Portfolio use tags

--- themes/argo/template-parts/content-portfolio.php
+++ themes/argo/template-parts/content-portfolio.php
@@ -1,18 +1,21 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> data-slide-in>
+	
 	<a href="<?php echo esc_url(get_permalink()) ?>">
-	<?php 
-		the_post_thumbnail('portfolio-square');
-	?>
+		<?php the_post_thumbnail('portfolio-square'); ?>
+	</a>
 
 	<div class="info">
 		<?php
 			the_title( '<h2 class="entry-title">', '</h2>' );
 		?>
-
-		<?php
-			the_excerpt();
-		?>	
+		<div class="tags">
+			<?php
+				$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'argo' ) );
+				if ( $tags_list ) {
+					echo $tags_list;
+				}
+			?>	
+		</div>
 	</div>
-	</a>
 </article>
