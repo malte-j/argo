5a809bd730856bfd14fa528dc965c4f0d5f96e11
luis-rieke <66038170+luis-rieke@users.noreply.github.com>
2020-12-12 18:40:48 +0100

    404 page

--- themes/argo/404.php
+++ themes/argo/404.php
@@ -14,20 +14,18 @@ get_header();
 
 		<section class="error-404 not-found">
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'argo' ); ?></h1>
+				<h1 class="page-title"><?php esc_html_e( 'Fehler 404: Diese Seite existiert nicht.', 'argo' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'argo' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
+				<p><?php esc_html_e( 'Es sieht ganz danach aus als würden die Ressourcen hier nicht mehr existieren. Vielleicht hat es diese Seite aber auch nie gegeben. Am besten besuchen Sie wieder eine der anderen Seiten und sollten Sie etwas vermissen, melden Sie sich gerne über die Kontaktseite.', 'argo' ); ?></p>
+				
+					<div class="wp-block-buttons">
+					<div class="wp-block-button"><a class="wp-block-button__link" href="/">Zurück zur Startseite</a></div>
+					</div>
 
 					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'argo' ); ?></h2>
+						<h2 class="widget-title"><?php esc_html_e( 'Vielleicht werden Sie hier fündig:', 'argo' ); ?></h2>
 						<ul>
 							<?php
 							wp_list_categories(
@@ -44,10 +42,7 @@ get_header();
 					</div><!-- .widget -->
 
 					<?php
-					/* translators: %1$s: smiley */
-					$argo_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'argo' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$argo_archive_content" );
-
+					/* Schlagwörter */
 					the_widget( 'WP_Widget_Tag_Cloud' );
 					?>
 
